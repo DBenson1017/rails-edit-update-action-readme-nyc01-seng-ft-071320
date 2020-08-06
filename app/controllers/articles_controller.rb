ed6c4bf25dd7e9c4c06973e721ebf1251842cfb6
DBenson1017 <devinbenson@gmail.com>
2020-08-06 09:15:13 -0400

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,5 +19,17 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
-  # add edit and update methods here
+  def edit #get action for the update form to user 
+    @article = Article.find(params[:id])
+  end 
+
+  def update #what sends the 'patch' the to db 
+  @article = Article.find(params[:id])
+  @article.update(title: params[:article][:title], description: params[:article][:description])
+  redirect_to article_path(@article)
+
+  end 
+
+
+
 end
